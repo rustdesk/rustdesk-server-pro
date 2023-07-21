9f252d66a3fdcba2acc2097b97f33e9b0a00f6ba
dinger1986 <daniel.lamb@flonix.co.uk>
2023-07-21 13:20:24 +0100

    Update convertfromos.sh

--- convertfromos.sh
+++ convertfromos.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# If any command fails, fail out
-set -e
-
 # This script will do the following to install RustDesk Server Pro replacing RustDesk Server Open Source
 # 1. Disable and removes the old services
 # 2. Install some dependencies
