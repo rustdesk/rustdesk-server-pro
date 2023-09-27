38d91fbeeb809651aebcc3194c048fcf16fffbdf
dinger1986 <daniel.lamb@flonix.co.uk>
2023-09-27 13:28:10 +0100

    Update convertfromos.sh

--- convertfromos.sh
+++ convertfromos.sh
@@ -284,6 +284,8 @@ rustdesknginx="$(
 server {
   server_name ${wanip};
       location / {
+           proxy_set_header        X-Real-IP       $remote_addr;
+           proxy_set_header        X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass http://127.0.0.1:21114/;
 }
 }
